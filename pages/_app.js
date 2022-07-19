5c3cff8e36074d39a953327b706f8e74742ed9ad
pulioliveto <emilianooliveto02@gmail.com>
2022-07-19 01:32:17 -0300

    solving css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootswatch/dist/lux/bootstrap.min.css'
 import '../styles/section.css'
+import '../styles/globals.css'
 import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
 	
